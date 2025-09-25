5d0877056f82c60321d84423b8132e1ef0e4e04e
Giuseppe GRASSO <giugrasso@outlook.be>
2025-09-25 21:50:45 +0200

    Add ai_actions file + rename aimodels to ai_models

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 
-from app.api.routes import admin, aimodels, games, players, scenarios
+from app.api.routes import admin, games, players, scenarios
 from app.core.db import init_db
 from app.initial_data import init_first_scenario, init_game_master
+from backend.app.api.routes import ai_models
 
 from .logging_config import logger
 
@@ -24,7 +25,7 @@ async def on_startup():
 
 
 app.include_router(admin.router, prefix="/v1", tags=["admin"])
-app.include_router(aimodels.router, prefix="/v1", tags=["aimodels"])
+app.include_router(ai_models.router, prefix="/v1", tags=["aimodels"])
 app.include_router(scenarios.router, prefix="/v1", tags=["scenarios"])
 app.include_router(games.router, prefix="/v1", tags=["games"])
 app.include_router(players.router, prefix="/v1", tags=["players"])
